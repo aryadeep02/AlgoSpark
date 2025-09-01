806f82a89590a2f2ffd4b545fc09007b42f9ed32
Aryadeep Varshney <152092016+aryadeep02@users.noreply.github.com>
2025-09-01 14:30:11 +0530

    Initial commit

--- src/pages/data-structures/index.ts
+++ src/pages/data-structures/index.ts
@@ -2,5 +2,6 @@ import ArraysPage from "./arrays";
 import StacksPage from "./stacks";
 import QueuesPage from "./queues";
 import LinkedListsPage from "./linked-lists";
+// import TreesPage from "./trees"
 
 export {ArraysPage,StacksPage,QueuesPage,LinkedListsPage};
--- src/pages/data-structures/queues.tsx
+++ src/pages/data-structures/queues.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { useState, useEffect } from "react"
 import DataStructureVisualizer from "./data-structure-visualizer"
 import { Input } from "@/components/ui/input"
